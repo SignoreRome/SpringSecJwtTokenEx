70c3aba5ead538e84bb4296165b1654efe8b25e4
Roman Volobuev <BroChilling@yandex.ru>
2021-01-23 21:00:51 +0700

    first step - template

---
+++ src/main/java/com/lolka/Springsecurityexample/model/Developer.java
@@ -0,0 +1,13 @@
+package com.lolka.Springsecurityexample.model;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class Developer {
+    private Long id;
+    private String firstName;
+    private String secondName;
+}
